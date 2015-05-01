6369c52a6ffe1b0ea8b979fa93d1bc8e51fe2bdc
Mariano Iglesias <mariano@cricava.com>
2015-05-01 15:43:41 -0300

    Adding missing changes to commands after introducing BaseJobModifierCommand

--- src/Command/FastAck.php
+++ src/Command/FastAck.php
@@ -1,36 +1,15 @@
 <?php
 namespace Disque\Command;
 
-use Disque\Exception;
-
-class FastAck extends BaseCommand implements CommandInterface
+class FastAck extends BaseJobModifierCommand implements CommandInterface
 {
     /**
-     * This command, with all its arguments, ready to be sent to Disque
+     * Get the command name
      *
-     * @param array $arguments Arguments
-     * @return array Command (separated in parts)
+     * @return string
      */
-    public function build(array $arguments)
+    protected function getCommand()
     {
-        if (empty($arguments)) {
-            throw new Exception\InvalidCommandArgumentException($this, $arguments);
-        }
-        return array_merge(['FASTACK'], $arguments);
-    }
-
-    /**
-     * Parse response
-     *
-     * @param mixed $response Response
-     * @return int Number of jobs deleted
-     * @throws Disque\Exception\InvalidCommandResponseException
-     */
-    public function parse($response)
-    {
-        if (!is_numeric($response)) {
-            throw new Exception\InvalidCommandResponseException($this, $response);
-        }
-        return (int) $response;
+        return 'FASTACK';
     }
 }
